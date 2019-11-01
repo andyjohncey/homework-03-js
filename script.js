a4ad36baaa63ef347516bb7d1be2072cc9063133
Andy Johncey <andrew.johncey@outlook.com>
2019-11-01 22:29:01 +1100

    add a space between key word and variable

--- script.js
+++ script.js
@@ -19,10 +19,10 @@ function generate() {
     console.log(charSize);
 
 
-    varupperCase = confirm("Do you want to add 'upercase letters' to your password?");
-    varlowerCase = confirm("Do you want to add 'lowercase letters' to your password?");
-    varnumChar = confirm("Do you want to add 'numbers' to your password?");
-    varspecChar = confirm("Do you want to add 'special characters' to your password?");
+    var upperCase = confirm("Do you want to add 'upercase letters' to your password?");
+    var lowerCase = confirm("Do you want to add 'lowercase letters' to your password?");
+    var numChar = confirm("Do you want to add 'numbers' to your password?");
+    var specChar = confirm("Do you want to add 'special characters' to your password?");
 
     upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
     lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
